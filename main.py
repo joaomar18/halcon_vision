38228ccd73121185d97f0d16e9fdbe51d13fc3c5
joaomar18 <joao.marques18jm@gmail.com>
2024-10-13 12:50:47 +0100

    up logging in some classes

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from vision.system import VisionSystem
 from vision.manager import VisionManager
 from communication.frontend import WebSocketServer
 import communication.queues as queues
+from db.client import DBClient
 import vision.construct
 import vision.data.comm
 import asyncio
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 PULLEY_CAMERA_PROGRAM_PATH = '/home/joao/Desktop/halcon_pulleys/hdevelop/PulleyCamera/inspect_pulleys.hdev'
 PULLEY_CAMERA_OUTPUT_PATH = '/home/joao/Desktop/halcon_pulleys/hdevelop/PulleyCamera/output/output_image'
@@ -13,8 +18,17 @@ FINAL_INSPECTION_CAMERA_OUTPUT_PATH = '/home/joao/Desktop/halcon_pulleys/hdevelo
 
 
 async def async_main():
+
+    db_client = DBClient(db_file='db/vision_app.db', 
+                         logger=logger)
     
-    frontend_server = WebSocketServer('localhost', 9999, queues.frontend_receive_queue, queues.frontend_send_queue)
+    frontend_server = WebSocketServer(host='localhost', 
+                                      port=9999, 
+                                      receive_queue=queues.frontend_receive_queue, 
+                                      send_queue=queues.frontend_send_queue,
+                                      db_client=db_client,
+                                      logger=logger)
+    
     vision_manager = VisionManager(queues.frontend_receive_queue, queues.frontend_send_queue)
     await vision_manager.add_vision_system(VisionSystem('PulleyCamera', 'Pulley Picking Camera', PULLEY_CAMERA_PROGRAM_PATH, PULLEY_CAMERA_OUTPUT_PATH, 
                                                         vision.construct.create_pulley_camera, 32, 1))
